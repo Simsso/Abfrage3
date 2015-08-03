a661d456f89ab848ed145cbe0dfd56ec295e56ec
Timo Denk <simssosdenk@googlemail.com>
2015-08-04 00:31:26 +0200

    New Mail class structure
    Added Default_Client_HTML_Mail class

--- mail.php
+++ mail.php
@@ -2,17 +2,27 @@ class Mail {
     const public DEFAULT_SENDER_EMAIL = "abfrage3@simsso.de";
 
     public $to;
-    public $sender;
     public $subject;
     public $body;
     public $header;
 
-    public function __construct($to, $from, $reply_to, $sender, $subject, $body) {
+    public function __construct($to, $from, $reply_to, $subject, $body) {
+    	// set to default value if no $from has been passed
+    	if (is_null($from)) {
+    		$from = self::DEFAULT_SENDER_EMAIL;
+    	}
+    	
+    	// set $reply_to to $from if no reply address has been passed
+    	if (is_null($reply_to)) {
+    		$reply_to = $from;
+    	}
+    	
+    	// set attributes
         this->$to = $to;
-        this->$sender = $sender;
         this->$subject = $subject;
         this->$body = $body;
 
+		// default header
         this->$header = "From: " . $from . "\r\nReply-To: " . $reply_to . "\r\n";
     }
     
@@ -23,8 +33,15 @@ class Mail {
 }
 
 class HTML_Mail extends Mail {
-    public function __construct($to, $from, $reply_to, $sender, $subject, $body) {
-        parent::__construct($to, $from, $reply_to, $sender, $subject, $body);
+    public function __construct($to, $from, $reply_to, $subject, $body) {
+    	// call default constructor
+        parent::__construct($to, $from, $reply_to, $subject, $body);
+        
+        // set different header with HTML information
         this->$header = "From: " . $from . "\r\nReply-To: " . $reply_to . "\r\nMIME-Version: 1.0\r\nContent-Type: text/html; charset=ISO-8859-1\r\n";
     }
+}
+
+class Default_Client_HTML_Mail {
+	public function __construct($to, $)
 }
