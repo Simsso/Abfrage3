abf63da2d888c6f5ab17d802fc100f82a839976f
Timo Denk <simssosdenk@googlemail.com>
2015-08-04 12:47:47 +0200

    Login page
    Style, HTML, include system for index.php file

--- index.php
+++ index.php
@@ -1,48 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Abfrage3</title>
-        <style type="text/css">
-            
-        </style>
-    </head>
-    <body>
-        <h2>Abfrage3</h2>
-        <h4>coming soon...</h4>
-        
-        
-        <!-- jquery -->
-        <script src="jquery-1.11.3.min.js" type="text/javascript"></script>
-        <script type="text/javascript">
-            (function() {
-                
-            })();
-        </script>
-        
-        
-        <!-- analytics -->
-        <!--<script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            
-            ga('create', 'UA-37082212-1', 'auto');
-            ga('send', 'pageview');
-        </script>-->
-        
-        <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
-        <script type="text/javascript">
-            window.cookieconsent_options = {
-                "message": "This website uses cookies to ensure you get the best experience on our website. ",
-                "dismiss": "Got it!",
-                "learnMore": "Learn more.",
-                "link": "https://en.wikipedia.org/wiki/HTTP_cookie",
-                "theme": "dark-bottom"
-            };
-        </script>
-        <script type="text/javascript" src="//s3.amazonaws.com/cc.silktide.com/cookieconsent.latest.min.js"></script>
-        <!-- End Cookie Consent plugin -->
-    </body>
-</html>
+<?php
+	if (isset($_SESSION['id'])) {
+		require('home.inc.php');
+	} else {
+		require('login.inc.php');
+	}
+?>
