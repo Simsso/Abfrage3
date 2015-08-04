0dd82e21dde60855a9868038c4d09b50548fed51
Timo Denk <simssosdenk@googlemail.com>
2015-08-04 11:58:20 +0200

    Login page
    Created login page grid, added images and include system for index file

---
+++ login.inc.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Abfrage3</title>
+        
+        <meta charset="utf-8">
+        <meta name="author" content="Timo Denk" />
+        <meta name="description" content="Abfrage3 is a online vocabulary trainer." />
+        <meta name="keywords" content="Timo, Denk, Abfrage3" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+        
+        <style type="text/css">
+        	* {
+        		margin: 0;
+        		padding: 0;
+        		font-family: Helvetica, Arial, sans-serif;        		
+        	}
+        	
+        	body {
+        		background-color: #ECEFF1;
+        	}
+        	
+        	.content-width {
+            	width: 1170px;
+            	margin: auto;
+        		
+        	}
+        	
+            .navbar {
+            	position: relative;
+            	
+            	height: 56px;
+            	width: 100%;
+            	
+            	padding: 5px;
+            	
+    			background-color: #8892BF;
+    			border-style: solid;
+    			border-width: 0 0 6px 0;
+            	border-color: #4F5B93;            	
+            }
+            .navbar .navbar-inner {
+            	position: relative;
+            }
+            .navbar .navbar-inner a {
+            	color: #E2E4EF;
+			    text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
+			    text-decoration: none;
+            }
+            .navbar .navbar-inner img {
+            	float: left;
+            	height: 56px;
+            	margin: 0 10px;
+            }
+            .navbar .navbar-inner .nav {
+            	position: relative;
+			    left: 0;
+			    float: left;
+			    margin: 0 10px 0 0;
+			    list-style: none;
+            }
+            .navbar .navbar-inner .nav li {
+            	float: left;
+			    margin: 22px 10px;
+			    line-height: 16px;
+			    font-size: 16px;
+            }
+            
+            body > .main {
+            	position: relative
+            }
+            
+            .box {
+            	margin: 25px 0;
+            	color: #263238;
+            	box-shadow: 0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);
+            }
+            .box .box-head {
+            	padding: 15px;
+            	background-color: #F3F5F6;
+    			border-bottom: 1px solid #ECEFF1;
+            }
+            .box .box-body {
+            	padding: 15px;
+            	background-color: #FFFFFF;
+            }
+        </style>
+    </head>
+    <body>
+    	<nav id="head-nav" class="navbar">
+    		<div class="navbar-inner content-width">
+    			<a href="/./">
+    				<img src="img/logo.png" />
+    			</a>
+    			<ul class="nav">
+    				<li><a href="#">About</a></li>
+    				<li><a href="#">Contact</a></li>
+    				<li><a href="#">Imprint</a></li>
+    			</ul>
+    		</div>
+    	</nav>
+    	
+    	<div class="main content-width">
+	        <div class="box">
+	        	<div class="box-head">What is Abfrage3?</div>
+	        	<div class="box-body">Abfrage3 is a web tool allowing users to enter vocabulary, share word lists and learn another language.</div>
+	        </div>
+        </div>
+        
+        
+        <!-- jquery -->
+        <script src="jquery-1.11.3.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            (function() {
+                
+            })();
+        </script>
+        
+        
+        <!-- analytics -->
+        <!--<script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            
+            ga('create', 'UA-37082212-1', 'auto');
+            ga('send', 'pageview');
+        </script>-->
+        
+        <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+        <script type="text/javascript">
+            window.cookieconsent_options = {
+                "message": "This website uses cookies to ensure you get the best experience on our website. ",
+                "dismiss": "Got it!",
+                "learnMore": "Learn more.",
+                "link": "https://en.wikipedia.org/wiki/HTTP_cookie",
+                "theme": "dark-bottom"
+            };
+        </script>
+        <script type="text/javascript" src="//s3.amazonaws.com/cc.silktide.com/cookieconsent.latest.min.js"></script>
+        <!-- End Cookie Consent plugin -->
+    </body>
