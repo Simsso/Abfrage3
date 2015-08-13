216f9380bfe8f7f4f1940131b716624015f7f657
Timo Denk <simssosdenk@googlemail.com>
2015-08-13 14:04:03 +0200

    Bugs

--- dbconnect.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$con = mysqli_connect("mysql16.1blu.de","s142619_2289845","sjdfkj3kjalsdf923jDASDJFASKsimssoAbfrage3Web","db142619x2289845");
-	
-	// check connection
-	if (mysqli_connect_errno()) {
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-?>
