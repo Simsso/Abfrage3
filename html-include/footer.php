6b10b3be914130f7ad858c9c9fdd17d593850c94
Timo Denk <simssosdenk@googlemail.com>
2015-11-09 19:41:15 +0100

    Manual language change support
    The use now has the ability to click on a language flag at the footer
    to switch between German and English.

--- html-include/footer.php
+++ html-include/footer.php
@@ -4,7 +4,17 @@
       <a href="#/about"><? echo $l['About']; ?></a> &middot; 
       <a href="#/tour"><? echo $l['Tour']; ?></a> &middot; 
       <a href="#/contact"><? echo $l['Contact']; ?></a> &middot; 
-      <a href="#/legal-info"><? echo $l['Legal_info']; ?></a><hr>&copy; Timo Denk
+      <a href="#/legal-info"><? echo $l['Legal_info']; ?></a>
+      <br><br>
+      <a href="?lang=en">
+      	<img src="img/flag-of-the-united-states.svg" class="language-flag" />
+      	English
+      </a> &middot; 
+      <a href="?lang=de">
+      	<img src="img/flag-of-germany.svg" class="language-flag" />
+      	Deutsch
+      </a>
+      <hr>&copy; Timo Denk
     </div>
   </div>
 </footer>
