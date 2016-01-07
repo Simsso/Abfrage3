ad74535136b60a2bae52e87b593554f6cb4a4871
Timo Denk <simssosdenk@googlemail.com>
2016-01-07 21:16:39 +0100

    Added visitors online script

--- scripts.js
+++ scripts.js
@@ -695,6 +695,15 @@ function getLocalId() {
 }
 
 
+// visitors online
+document.write('\x3Cscript src="https://cdn.socket.io/socket.io-1.3.7.js" type="text/javascript">\x3C/script>');
+document.write('\x3Cscript src="http://const.timodenk.com/visitors-online/client.js" type="text/javascript">\x3C/script>');    
+// number of visitors has changed
+window.visitorsOnlineChanged = function(count) {
+  console.log(count);
+};
+
+
 // Google analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-37082212-1', 'auto');
